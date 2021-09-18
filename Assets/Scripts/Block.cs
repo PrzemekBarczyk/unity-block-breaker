8e2add1563b6320670f8c48997cb9c195774af5d
PrzemekBarczyk <przemek.barczyk@gmail.com>
2021-09-18 19:47:18 +0200

    Score

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,6 +4,15 @@ public class Block : MonoBehaviour
 {
 	[SerializeField] [Min(1)] int _strength = 1;
 
+	[SerializeField] int _score = 1;
+
+	GameManager _gameManager;
+
+	void Start()
+	{
+		_gameManager = GameManager.Instance;
+	}
+
 	void OnCollisionEnter2D(Collision2D collision)
 	{
 		if (collision.transform.CompareTag("Ball"))
@@ -18,6 +27,7 @@ public class Block : MonoBehaviour
 
 		if (_strength == 0)
 		{
+			_gameManager.AddScore(_score);
 			Destroy(gameObject);
 		}
 	}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,23 +3,32 @@ using UnityEngine.UI;
 
 public class GameManager : MonoSingleton<GameManager>
 {
-	[SerializeField] [Min(1)] int _livesValue = 3;
+	[SerializeField] [Min(0)] int _scoreValue = 0;
+	[SerializeField] Text _scoreText;
 
+	[SerializeField] [Min(1)] int _livesValue = 3;
     [SerializeField] Text _livesText;
 
-	[SerializeField] Ball _ball;
-	[SerializeField] Paddle _paddle;
+	Ball _ball;
+	Paddle _paddle;
 
 	SceneManager _sceneManager;
 
 	void Start()
 	{
+		_scoreText.text = _scoreValue.ToString();
 		_livesText.text = _livesValue.ToString();
 
 		_ball = FindObjectOfType<Ball>();
 		_paddle = FindObjectOfType<Paddle>();
 
 		_sceneManager = SceneManager.Instance;
+	}
+
+	public void AddScore(int scoreToAdd)
+	{
+		_scoreValue += scoreToAdd;
+		_scoreText.text = _scoreValue.ToString();
 	}
 
 	public void RemoveLife()
