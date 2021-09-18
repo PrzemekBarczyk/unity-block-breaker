e5533f35ef857b311acb71bd6f3d6cfcd98b826e
PrzemekBarczyk <przemek.barczyk@gmail.com>
2021-09-18 14:44:26 +0200

    Ball movement

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Ball : MonoBehaviour
+{
+	[SerializeField] [Range(100, 400)] float _speed = 100f;
+	[Space]
+
+	[SerializeField] [Range(200, 400)] float _bounceOfPlayerRange = 300f;
+	[Space]
+
+	[SerializeField] Paddle _paddle;
+
+	Vector3 _offsetToPlayer;
+
+	bool _isGluedToPlayer = true;
+
+	Rigidbody2D _myRigidbody;
+
+	void Awake()
+	{
+		_myRigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void Start()
+	{
+		_offsetToPlayer = _paddle.transform.position - transform.position;
+	}
+
+	void Update()
+	{
+		if (_isGluedToPlayer)
+		{
+			FollowPlayer();
+		}
+		if (Input.GetMouseButtonDown(0))
+		{
+			_isGluedToPlayer = false;
+		}
+	}
+
+	void FollowPlayer()
+	{
+		transform.position = _paddle.transform.position - _offsetToPlayer;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.transform.CompareTag(_paddle.tag))
+		{
+			float xForce = (transform.position.x - collision.transform.position.x) * _bounceOfPlayerRange;
+			float yForce = Mathf.Sqrt(Mathf.Abs(_speed * _speed - xForce * xForce));
+
+			_myRigidbody.velocity = Vector2.zero;
+			_myRigidbody.AddForce(new Vector2(xForce, yForce));
+		}
+	}
+}
